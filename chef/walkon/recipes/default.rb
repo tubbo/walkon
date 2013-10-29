7a596eb81410daeca45641d0ef411aee8bc2a65f
Tom Scott <tubbo@psychedeli.ca>
2013-10-28 22:16:50 -0400

    Add upstart job to Chef cookbook, and add directives to Chef install

--- chef/walkon/recipes/default.rb
+++ chef/walkon/recipes/default.rb
@@ -2,7 +2,26 @@
 # Cookbook Name:: walkon
 # Recipe:: default
 #
-# Copyright (C) 2013 YOUR_NAME
+# Copyright (C) 2013 Tom Scott
 # 
 # All rights reserved - Do Not Redistribute
 #
+
+gem_package "install the latest gem codebase" do
+  package_name 'walkon'
+  action :install
+end
+
+cookbook_file "install the upstart job" do
+  source "upstart.conf"
+  path "/etc/init/walkon.conf"
+  mode '0755'
+  user 'root'
+  group 'root'
+  action :create
+end
+
+service "start the walkon process" do
+  service_name 'walkon'
+  action :restart
+end
